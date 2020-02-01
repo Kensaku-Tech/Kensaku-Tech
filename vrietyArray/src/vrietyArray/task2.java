7bf64a3761a963d4e87885b461f88d86f74e43c1
Kensaku-Tech <itohamaken21@gmail.com>
2020-02-01 14:39:47 +0900

    課題再提出

--- vrietyArray/src/vrietyArray/task2.java
+++ vrietyArray/src/vrietyArray/task2.java
@@ -7,27 +7,36 @@ public abstract class task2 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		ArrayList <Integer> G = new ArrayList<>();
-		ArrayList <Integer> K = new ArrayList<>();
+		ArrayList <Integer> num = new ArrayList<>();
 
-		K.add(0,21);
-		K.add(1,3);
-		K.add(2,99);
-		K.add(3,51);
-		K.add(4,1);
-		K.add(5,87);
-		K.add(6,11);
+		num.add(0,21);
+		num.add(1,3);
+		num.add(2,32);
+		num.add(3,6);
+		num.add(4,99);
+		num.add(5,72);
+		num.add(6,78);
+		num.add(7,51);
+		num.add(8,1);
+		num.add(9,26);
+		num.add(10,87);
+		num.add(11,11);
+		num.add(12,48);
+		num.add(13,60);
 
-		G.add(0,32);
-		G.add(1,6);
-		G.add(2,72);
-		G.add(3,78);
-		G.add(4,26);
-		G.add(5,48);
-		G.add(6,60);
+		ArrayList<Integer> G = new ArrayList<>();
+		ArrayList<Integer> K = new ArrayList<>();
 
-		System.out.println("奇数:" + K);
-		System.out.println("偶数:" + G);
+		for(int i = 0; i <= 13; i++) {
+			int q = num.get(i);
+			if(q % 2 == 0) {
+				G.add(q);
+			}else {
+				K.add(q);
+			}
+		}
+	System.out.println("奇数:" + K);
+	System.out.println("偶数:" + G);
 
 
 	}
--- vrietyArray/src/vrietyArray/task4.java
+++ vrietyArray/src/vrietyArray/task4.java
@@ -8,37 +8,45 @@ public class task4 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		ArrayList <Integer>num = new ArrayList<>();
+
+		num.add(0,21);
+		num.add(1,3);
+		num.add(2,32);
+		num.add(3,6);
+		num.add(4,99);
+		num.add(5,72);
+		num.add(6,78);
+		num.add(7,51);
+		num.add(8,1);
+		num.add(9,26);
+		num.add(10,87);
+		num.add(11,11);
+		num.add(12,48);
+		num.add(13,60);
+		num.add(14,29);
+		num.add(15,5);
+		num.add(16,46);
+		num.add(17,74);
+		num.add(18,14);
+		num.add(19,193);
+		num.add(20,215);
+		num.add(21,83);
+
 		ArrayList <Integer> G = new ArrayList<>();
-		G.add(0,32);
-		G.add(1,6);
-		G.add(2,72);
-		G.add(3,78);
-		G.add(4,26);
-		G.add(5,48);
-		G.add(6,60);
-		G.add(7,46);
-		G.add(8,74);
-		G.add(9,14);
-
 		ArrayList <Integer> K = new ArrayList<>();
-		K.add(0,21);
-		K.add(1,3);
-		K.add(2,99);
-		K.add(3,51);
-		K.add(4,1);
-		K.add(5,87);
-		K.add(6,11);
-		K.add(7,29);
-		K.add(8,5);
-		K.add(9,193);
-		K.add(10,215);
-		K.add(11,83);
-
-
 		HashMap<String,ArrayList<Integer>> number = new HashMap<>();
 
-		number.put("偶数", G);
-		number.put("奇数", K);
+		for(int i = 0; i <= 21; i++) {
+			int q = num.get(i);
+			if(q % 2 == 0) {
+				G.add(q);
+				number.put("偶数", G);
+			}else {
+				K.add(q);
+				number.put("奇数", K);
+			}
+		}
 
 		System.out.print(number);
 
